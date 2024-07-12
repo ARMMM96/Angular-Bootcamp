f3a1b0a9c7c5678afe2df947c2f9f359835d74bf
Ahmed Rabie <ahmed.r@trusstedge.com>
2024-07-12 21:37:47 +0300

    Form validation implemented

--- Tasks/1-InvestmentCalculator/src/app/components/investment-form-input/investment-form-input.component.ts
+++ Tasks/1-InvestmentCalculator/src/app/components/investment-form-input/investment-form-input.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { InvestmentResultsServiceTsService } from '../../services/investment-results.service.ts.service';
 import type { InvestmentResult } from '../../interfaces/investment-results.interface';
 
 @Component({
   selector: 'app-investment-form-input',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './investment-form-input.component.html',
   styleUrl: './investment-form-input.component.css',
 })
@@ -16,19 +17,30 @@ export class InvestmentFormInputComponent {
   enteredAnnualInvestment: number = 0;
   enteredExpectedReturn: number = 0;
   enteredDuration: number = 0;
+  submissionError: boolean = false;
 
   constructor(
     private investmentResultsService: InvestmentResultsServiceTsService
   ) {}
 
   onSubmit() {
+    if (
+      this.enteredInitialInvestment < 1 ||
+      this.enteredAnnualInvestment < 1 ||
+      this.enteredExpectedReturn < 1 ||
+      this.enteredDuration < 1
+    ) {
+      this.submissionError = true;
+      return;
+    }
+
+    this.submissionError = false;
     let results = this.investmentResultsService.calculateInvestimentResults({
       initialInvestment: this.enteredInitialInvestment,
       annualInvestment: this.enteredAnnualInvestment,
       expectedReturn: this.enteredExpectedReturn,
       duration: this.enteredDuration,
     });
-
 
     this.send.emit(results);
   }
